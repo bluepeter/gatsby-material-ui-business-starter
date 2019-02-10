4eddfa743ec3a46dc2a53b15ee147f1d1b63fd48
Peter Bray <petertheyeti@gmail.com>
2019-02-10 09:50:22 -0800

    Remove custom font. Too much bloat.

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,16 +29,5 @@ module.exports = {
         name: "pages",
       },
     },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: "Roboto",
-            variants: ["300", "400", "500"],
-          },
-        ],
-      },
-    },
   ],
 };
